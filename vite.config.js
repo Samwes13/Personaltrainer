5d85fa4053a201f378f68d6fd7e1add6c45a1fba
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-12-03 20:42:46 +0200

    ghanges

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactapp/', // Add this row and use your own repository name
   plugins: [react()],
-})
+  test: {
+  globals: true,
+  environment: 'jsdom',
+  },
+  })
+
