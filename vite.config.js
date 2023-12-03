3c253ffc8598bcac2ba8d57fea93311e3cca5d2e
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-12-03 21:04:20 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactapp/', // Add this row and use your own repository name
+  base: '/personaltrainer/', // Add this row and use your own repository name
   plugins: [react()],
   test: {
   globals: true,
